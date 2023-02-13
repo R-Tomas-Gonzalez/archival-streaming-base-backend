65874b01c040bec88de8415a7adf1157125b7643
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2023-02-13 08:05:51 -0600

    update

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
@@ -15,4 +25,6 @@ app.use(express.json())
 const moviesRouter = require('./routes/movies')
 app.use('/movies', moviesRouter)
 
-app.listen(3000, () => console.log('Server Started'))
+connectDB().then(() => {
+    app.listen(3000, () => console.log('Server Started'))
+})
