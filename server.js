d7b9151bd864cbb2b67be853997999bea2332a56
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2023-03-08 16:59:13 -0600

    added mongo options

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     } catch (error) {
         console.log(error);
         process.exit(1);
